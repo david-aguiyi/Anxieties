c7281930ee7f805608448a1c3bcf716a82ae51b1
david-aguiyi <davidaguiyi9@gmail.com>
2023-03-22 16:01:37 +0100

    Created Hate button

--- card.js
+++ card.js
@@ -23,3 +23,25 @@ const handleLoveClick = () => {
         activeIndex = nextIndex;
     });
 }
+
+const handleHateClick = () => {
+    // Bump next Index
+
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`);
+    const nextGroup = document.querySelector(`[data-Index="${nextIndex}"]`);
+
+    // Active group becomes after
+
+    currentGroup.dataset.status = 'before';
+
+    //Next group becomes active
+    
+    nextGroup.dataset.status = 'becoming-active-from-after';
+
+    setTimeout(() => {
+        nextGroup.dataset.status = 'active'
+        activeIndex = nextIndex;
+    });
+}
